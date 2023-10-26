812256c9050b450834248acc3343ac1459e075d8
qWitLy <soldatenko_2003@mail.ru>
2023-10-26 19:05:44 +0800

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebApplication10.Data;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationContext ac = new(new DbContextOptionsBuilder<ApplicationContext>()
+            .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MVCDB;Trusted_Connection=True;").Options);
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult GetUsers()
+        {
+            return View(ac.Users);
+        }
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Change(int id)
+        {
+            User? user = ac.Users.FirstOrDefault(data => data.Id == id);
+            if (user == null) return View("GetUsers", ac.Users);
+            return View(user);
+        }
+        [HttpPost]
+        public IActionResult Change(User user)
+        {
+            ac.Users.Update(user);
+            ac.SaveChanges();
+            return View("GetUsers",ac.Users);
+
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            User? user = ac.Users.Find(id);
+            if(user==null)
+            return View("GetUsers",ac.Users);
+            ac.Users.Remove(user);
+            ac.SaveChanges();
+            return View("GetUsers",ac.Users);
+        }
+        [HttpPost]
+        public IActionResult Check(User user)
+        {
+            Console.Write(user.Name + user.Age);
+            ac.Add(user);
+            ac.SaveChanges();
+            return View("GetUsers",ac.Users);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication10.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Display(Name="Имя")]
+        [Required(ErrorMessage ="Поле не должно быть пустым")]
+        public string Name { get; set; }
+        [Display(Name="Возраст")]
+        [Required(ErrorMessage ="Поле не должно быть пустым")]
+        public int Age { get; set; }
+    }
+}
